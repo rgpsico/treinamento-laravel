136310de847f7e8a3d1c8104de7acb9a4ac0f6c5
roger <rogernevesn@gmail.com>
2023-01-15 01:03:34 -0300

    ajuste

---
+++ resources/views/novo/_partials/servicoscomponent.blade.php
@@ -0,0 +1,48 @@
+<section class="categories">
+    <div class="container"> 
+      <!-- Row  -->
+      <div class="row justify-content-center">
+        <div class="col-md-7 text-center m-b-10">
+          <h2 class="title">Serviços</h2>
+          <h6 class="subtitle">Temos o profissional que você precisa</h6>
+        </div>
+      </div>
+      <!-- Row  -->
+      <div class="row">
+        <div class="col-md-12">
+          <ul class="list-unstyled text-center p-t-30">
+            <li><a href="#"><img src="images/Vehicles.png" alt="Classified Plus"/>
+              <p> Vehicles </p>
+              </a> </li>
+            <li><a href="#"><img src="images/Electronics.png" alt="Classified Plus"/>
+              <p> Electronics </p>
+              </a> </li>
+            <li><a href="#"><img src="images/Mobiles.png" alt="Classified Plus"/>
+              <p>Mobiles </p>
+              </a> </li>
+            <li><a href="#"><img src="images/Furniture.png" alt="Classified Plus"/>
+              <p> Furniture</p>
+              </a> </li>
+            <li><a href="#"><img src="images/Fashion.png" alt="Classified Plus"/>
+              <p> Fashion </p>
+              </a> </li>
+            <li><a href="#"><img src="images/Real-Estate.png" alt="Classified Plus"/>
+              <p> Real Estate </p>
+              </a> </li>
+            <li><a href="#"><img src="images/Animals.png" alt="Classified Plus/">
+              <p> Animals </p>
+              </a> </li>
+            <li><a href="#"><img src="images/Education.png" alt="Classified Plus"/>
+              <p>Education </p>
+              </a> </li>
+            <li><a href="#"><img src="images/Baby-products.png" alt="Classified Plus"/>
+              <p>Baby products</p>
+              </a> </li>
+            <li><a href="#"> <img src="images/Services.png" alt="Classified Plus"/>
+              <p>Services </p>
+              </a> </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
---
+++ resources/views/novo/_partials/slider.blade.php
@@ -0,0 +1,48 @@
+<section class="slider">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+             <img src="{{asset('images/chave.jpg')}}" height="10%" style=" max-width: 100%;
+             max-height: 20%;  zoom: 50%;" alt="Classified Plus" class="slide-image">
+          <div class="slide-text">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <h1>Encontre seu novo lar aqui</h1>
+                  <p>Anuncie seu imóvel conosco, nós cuidamos do resto</p>
+                  <div>
+                    <form class="book-now-home">
+                      <div class="form-group">
+                        <input type="email" class="form-control text-truncate" placeholder="What are you looking for">
+                      </div>
+                      <div class="form-group selectdiv">
+                        <select class="form-control text-truncate">
+                          <option>Nossa Localização</option>
+                          
+                        </select>
+                      </div>
+                      <div class="form-group selectdiv">
+                        <select class="form-control border-right-0 text-truncate">
+                          <option>Selecionar Categoria</option>
+            
+                        </select>
+                      </div>
+                      <button type="submit" class="btn btn-primary booknow btn-skin">Search Now</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a> </div>
+  </section>
+  <!-- End Slider --> 
+  
