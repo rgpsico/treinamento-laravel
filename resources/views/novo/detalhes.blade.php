639e959c46745cd709cffff9cfe923fb51fe76c2
roger <rogernevesn@gmail.com>
2023-01-19 20:20:47 -0300

    ajuste

--- resources/views/novo/detalhes.blade.php
+++ resources/views/novo/detalhes.blade.php
@@ -5,12 +5,12 @@
     <div class="row">
       <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
         <div class="detail_box"> 
-          <img class="img-fluid" src="{{ asset('imagens/imoveis/'.$datas->gallery[0]->image) }}" alt="{{$datas->title}}">
+          <img class="img-fluid" src="{{ asset('imagens/imoveis/'.$data->gallery[0]->image) }}" alt="{{$data->title}}">
           <div class="m-t-20">
             <ul class="owl-carousel list-unstyled m-b-0" id="product_slider">
             
-              @foreach ($datas->gallery as $key => $gallery ) 
-                    <li> <img class="img-fluid" src="{{ asset('imagens/imoveis/'.$datas->gallery[$key]->image) }}" alt="slide {{$key}}"> </li>
+              @foreach ($data->gallery as $key => $gallery ) 
+                    <li> <img class="img-fluid" src="{{ asset('imagens/imoveis/'.$data->gallery[$key]->image) }}" alt="slide {{$key}}"> </li>
               @endforeach
             </ul>
           </div>
@@ -19,14 +19,14 @@
       <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12">
         <div class="detail_box">
           <div class="detail_head">
-            <h3> {{$datas->title}}<br>
+            <h3> {{$data->title}}<br>
                </h3>
-            <p>{{$datas->description}} </p>
+            <p>{{$data->description}} </p>
             <ul class="list-unstyled text-capitalize m-b-0 m-t-15">
               <li class="d-inline-block p-r-20">
                 <a href="#"> 
                   <i class="fa fa-clock-o">
-                    </i> <span>{{$datas->data_created}}</span>
+                    </i> <span>{{$data->data_created}}</span>
                   </a> 
                   </li>
               <li class="d-inline-block p-r-20">
@@ -59,7 +59,7 @@
                 <div class="form-check">
                   <input class="form-check-input" value="" type="checkbox">
                   <label class="form-check-label"> </label>
-                  <img src="{{asset('images/warrenty.png')}}" alt="{{$datas->title}}">
+                  <img src="{{asset('images/warrenty.png')}}" alt="{{$data->title}}">
                   <div class="warranty d-inline-block">
                     Não precisa de deposito*<br>
                      </div>
@@ -69,7 +69,7 @@
                 <div class="form-check">
                   <input class="form-check-input" value="" type="checkbox">
                   <label class="form-check-label"> </label>
-                  <img src="{{asset('images/insurance.png')}}" alt="{{$datas->title}}">
+                  <img src="{{asset('images/insurance.png')}}" alt="{{$data->title}}">
                   <div class="warranty d-inline-block">10 minutos da praia<br>
                     </div>
                 </div>
@@ -79,13 +79,13 @@
           <div class="detail_prize p-t-10">
             <ul class="list-unstyled">
               <li class="d-inline-block pr-3"> Preço Mensal : </li>
-              <li class="d-inline-block Price_m"> {{$datas->price}}</li>
+              <li class="d-inline-block Price_m"> {{$data->price}}</li>
             </ul>
           </div>
           
           <div class="detail_btn d-flex m-t-20">
             <button class="btn_chat w-100 text-white mr-3 py-2 border-0" type="submit" value="button">
-              <a href="https://api.whatsapp.com/send?phone={{$datas->user->phone}}" target="_blank" style="color:#fff; text-decoration: none;">
+              <a href="https://api.whatsapp.com/send?phone={{$data->user->phone}}" target="_blank" style="color:#fff; text-decoration: none;">
                 <i class="fa fa-comment-o"></i> Chamar
               </a>
             </button>
@@ -113,7 +113,7 @@
     <div class="row">
       <div class="col-md-9">
         <div class="description_box">
-          <p>{{$datas->description}} </p>
+          <p>{{$data->description}} </p>
         
       </div>
     </div>
@@ -139,7 +139,7 @@
     </div>
     <!-- Row  -->
     
-    @include('novo.components.listagemComponent')
+   
     <div class="row">
            
         <div class="col-md-12">
