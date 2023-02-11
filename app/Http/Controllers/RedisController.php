d5e0608142e3809bbffad2e46ad4dbe236bf0db0
roger <rogernevesn@gmail.com>
2023-02-11 02:37:08 -0300

    ajuste

--- app/Http/Controllers/RedisController.php
+++ app/Http/Controllers/RedisController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Models\Imovel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Redis;
 
 class RedisController extends Controller
 {
    public function index()
-   {    
-    return Imovel::all();
+   {
+      return Cache::remember('imovel', 60, function () {
+       Imovel::all();
+    });
    }
 }
