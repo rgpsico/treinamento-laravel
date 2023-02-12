3a6e692a106d1bdc9ea5e8262143ba11cbb5f525
roger <rogernevesn@gmail.com>
2023-02-12 00:13:28 -0300

    ajuste

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -70,7 +70,7 @@
 
       <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
-          <a href="/" class="brand-link bg-light">
+          <a href="/" class="brand-link">
               <img src="{{asset('images/logo2.png')}}" 
               alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
                   style="opacity: .8">
