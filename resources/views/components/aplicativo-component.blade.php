0756c829d3036e1408ce65f437e481248b0675f1
roger <rogernevesn@gmail.com>
2023-02-18 02:14:29 -0300

    ajusyte

---
+++ resources/views/components/aplicativo-component.blade.php
@@ -0,0 +1,25 @@
+<section class="app_store m-t-50">
+    <div class="container">
+        <!-- Row  -->
+        <div class="row justify-content-center">
+            <div class="col-md-10 text-center">
+                <h2 class="title">Download on App Store</h2>
+                <div class="clearfix"></div>
+                <h6 class="subtitle">There are many variations of passages of Lorem Ipsum available, but the
+                    majority have suffered alteration in some form, by injected humour, or randomised words which
+                    don't look even slightly believable.</h6>
+            </div>
+        </div>
+        <!-- Row  -->
+        <div class="row">
+            <div class="app_parts ">
+                <button class="app-btn btn" type="submit" value="butten"><i class="fa fa-apple app-icon "></i>
+                    Apple Store </button>
+                <button class="app-btn btn" type="submit" value="butten"><i class="fa fa-android app-icon"></i>
+                    Apple Store </button>
+                <button class="app-btn btn" type="submit" value="butten"><i class="fa fa-windows app-icon"></i>
+                    Apple Store </button>
+            </div>
+        </div>
+    </div>
+</section>
--- resources/views/novo/categoria.blade.php
+++ resources/views/novo/categoria.blade.php
@@ -71,83 +71,11 @@
     <!-- End Categories -->
 
     <!-- App_Store -->
-    <section class="app_store m-t-50">
-        <div class="container">
-            <!-- Row  -->
-            <div class="row justify-content-center">
-                <div class="col-md-10 text-center">
-                    <h2 class="title">Download on App Store</h2>
-                    <div class="clearfix"></div>
-                    <h6 class="subtitle">There are many variations of passages of Lorem Ipsum available, but the
-                        majority have suffered alteration in some form, by injected humour, or randomised words which
-                        don't look even slightly believable.</h6>
-                </div>
-            </div>
-            <!-- Row  -->
-            <div class="row">
-                <div class="app_parts ">
-                    <button class="app-btn btn" type="submit" value="butten"><i class="fa fa-apple app-icon "></i>
-                        Apple Store </button>
-                    <button class="app-btn btn" type="submit" value="butten"><i class="fa fa-android app-icon"></i>
-                        Apple Store </button>
-                    <button class="app-btn btn" type="submit" value="butten"><i class="fa fa-windows app-icon"></i>
-                        Apple Store </button>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-AplicativoComponent />
     <!-- End App_Store -->
 
     <!-- Testimonial -->
-    <section class="testimonials">
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-lg-5 text-center">
-                    <h2 class="title">Testimonials</h2>
-                    <h6 class="subtitle">What Our Clients Says</h6>
-                </div>
-            </div>
-            <div id="carouselExampleIndicators2" class="carousel slide" data-ride="carouse2">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators2" data-slide-to="0" class=""><img
-                            class="testimonial-image" src="images/testimonials_2.png" alt="Classified Plus"></li>
-                    <li data-target="#carouselExampleIndicators2" data-slide-to="1" class="active"><img
-                            class="testimonial-image" src="images/testimonials_1.png" alt="Classified Plus"></li>
-                    <li data-target="#carouselExampleIndicators2" data-slide-to="2"><img class="testimonial-image"
-                            src="images/testimonials_3.png" alt="Classified Plus"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                            been the industry's<br>
-                            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                            scrambled it to<br>
-                            make a type specimen book. It has survived not only five centuries, </p>
-                        <h3>Williams Sherry</h3>
-                        <h4>User</h4>
-                    </div>
-                    <div class="carousel-item active">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                            been the industry's<br>
-                            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                            scrambled it to<br>
-                            make a type specimen book. It has survived not only five centuries, </p>
-                        <h3>Williams Sherry</h3>
-                        <h4>User</h4>
-                    </div>
-                    <div class="carousel-item">
-                        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-                            been the industry's<br>
-                            standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
-                            scrambled it to<br>
-                            make a type specimen book. It has survived not only five centuries, </p>
-                        <h3>Williams Sherry</h3>
-                        <h4>User</h4>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
+    <x-DepoimentoComponent />
     <!-- End Testimonial -->
 
     <!-- End Testimonial -->
