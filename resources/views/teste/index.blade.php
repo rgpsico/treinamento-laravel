cd8847b192a67c45dae179f38f424bbc6a1b6359
roger <rogernevesn@gmail.com>
2023-03-11 02:25:52 -0300

    ajuste

--- resources/views/teste/index.blade.php
+++ resources/views/teste/index.blade.php
@@ -2,80 +2,70 @@
 
 
 @section('content')
-    <div class="col-12">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Imoveis</h3>
-                <div class="card-tools">
-                    <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                        <i class="fas fa-minus"></i>
-                    </button>
-                    <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                        <i class="fas fa-times"></i>
-                    </button>
-                </div>
+    <h1>Users</h1>
+    <section class="container-fluid p-4">
+
+        <div class="d-flex justify-content-end">
+            <button class="btn btn-success mb-3">
+                <i class="fas fa-user-plus"></i> Cadastrar
+            </button>
+        </div>
+
+        <div class="row">
+            <div class="form-group col-12 col-md-3">
+
+                <select name="" id="" class="form-control">
+                    <option value="">Redes</option>
+
+                </select>
             </div>
-            <div class="card-body p-0" style="display: block;">
-                <table class="table table-striped projects">
-                    <thead>
-                        <tr>
-                            <th style="width: 1%">
-                                #
-                            </th>
-                            <th style="width: 20%">
-                                foto
-                            </th>
-                            <th style="width: 30%">
-                                Local
-                            </th>
-                            <th>
-                                Preço
-                            </th>
-                            <th style="width: 8%" class="text-center">
-                                Status
-                            </th>
-                            <th style="width: 20%; text-align:center;">
-                                Ações
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody class="dados">
-                        <tr>
-                            <td>10</td>
-                            <td><img src="" alt=""></td>
-                            <td>Rampinha</td>
-                            <td>400</td>
-                            <td> <span class="badge badge-pill badge-danger badge-3">Alugado</span>
-                            </td>
 
+            <div class="form-group col-12 col-md-3">
 
-                            <td class="project-actions text-right">
-                                <a class="btn btn-primary btn-sm" href="#">
-                                    <i class="fas fa-folder">
-                                    </i>
-                                    Ver
-                                </a>
-                                <a class="btn btn-info btn-sm" href="#">
-                                    <i class="fas fa-pencil-alt">
-                                    </i>
-                                    Editar
-                                </a>
-                                <a class="btn btn-danger btn-sm" href="#">
-                                    <i class="fas fa-trash">
-                                    </i>
-                                    Excluir
-                                </a>
-                            </td>
-                            </td>
-                        </tr>
+                <select name="" id="" class="form-control">
+                    <option value="">Redes</option>
+                </select>
+            </div>
 
+            <div class="form-group col-12 col-md-3">
 
-                    </tbody>
-                </table>
+                <select name="" id="" class="form-control">
+                    <option value="">Redes</option>
+                </select>
+            </div>
+
+            <div class="form-group col-12 col-md-3">
+
+                <select name="" id="" class="form-control">
+                    <option value="">Redes</option>
+                </select>
             </div>
         </div>
 
+        <table class="table table-striped table-advance table-hover">
+            <thead>
+                <tr>
+                    <th><i class="fa fa-bullhorn"></i> Company</th>
+                    <th class="hidden-phone"><i class="fa fa-question-circle"></i> Descrition</th>
+                    <th><i class="fa fa-bookmark"></i> Profit</th>
+                    <th><i class=" fa fa-edit"></i> Status</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><a href="#">Vector Ltd</a></td>
+                    <td class="hidden-phone">Lorem Ipsum dorolo imit</td>
+                    <td>12120.00$ </td>
+                    <td><span class="badge badge-info label-mini">Due</span></td>
+                    <td>
+                        <button class="btn btn-success btn-sm"><i class="fa fa-check"></i></button>
+                        <button class="btn btn-primary btn-sm"><i class="fa fa-pencil"></i></button>
+                        <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i></button>
+                    </td>
+                </tr>
 
-
-        </section>
-    @endsection
+            </tbody>
+        </table>
+    </section>
+@endsection
