c7d194970fd40a478fa996ef5d2c66dfcd1e5c79
roger <rogernevesn@gmail.com>
2023-05-10 21:06:24 -0300

    ajuste

--- resources/views/public/imoveis/show.blade.php
+++ resources/views/public/imoveis/show.blade.php
@@ -33,7 +33,6 @@
                         @endif
                         <div class="m-t-20">
                             <ul class="owl-carousel list-unstyled m-b-0" id="product_slider">
-
                                 @if (isset($data->gallery))
                                     @foreach ($data->gallery as $key => $gallery)
                                         <li> <img class="img-fluid thumb-gallery"
@@ -49,8 +48,7 @@
                 <div class="col-xl-6 col-lg-6 col-md-6 col-sm-12 col-12 ">
                     <div class="detail_box">
                         <div class="detail_head">
-                            <h1> {{ $data->title }}  
-                                <span style="font-size:29px; text-align: center; color:red; font-weight:bold; ">{{$data->venda == 1 ? '(Venda)' : '(Aluguel)'}}</span><br>
+                            <h1> {{ $data->title }} <span style="font-size:29px; text-align: center; color:red; font-weight:bold; ">{{$data->venda == 1 ? '(Venda)' : '(Aluguel)'}}</span><br>
                             </h1>
                             <p style="text-transform: capitalize; margin-right:10px;">{{ $data->description }} </p>
                             <ul class="list-unstyled text-capitalize m-b-0 m-t-15">
@@ -62,54 +60,26 @@
                                 </li>
                             </ul>
                         </div>
+
+
                         <ul class="list-unstyled d-inline-block float-left detail_left m-b-0 col-7">                                               
                             <li style="font-size:20px; margin-bottom:10px; margin-top:15px;">    
                             <i class="fas fa-money-bill"></i> {{ $data->deposito == 0 ? 'Não precisa de deposito*' : 'Precisa de deposito' }}
                         </li> 
                             <li style="my-1"><i class="fas fa-sun"></i> <div class="warranty d-inline-block"> 10 minutos da praia<br></div></li> 
-                            <li class="d-inline-block pr-3 detail_prize my-3" style="font-size:20px;"> Preço Mensal : </li>
-                            <li class="d-inline-block Price_m detail_prize text-dark font-weight-bold" style="font-size:40px; "> R$ {{ str_replace('.',',', $data->price) }}</li>
-                            <li class="d-inline-block Price_m detail_prize text-dark font-weight-bold">
-                            <div class="detail_btn d-flex m-t-10">
-                            <button class="btn_chat w-100 text-white mr-1 py-1 border-0" 
-                            type="submit" 
-                            value="button">
-                                <a href="https://api.whatsapp.com/send?phone={{ $data->user->phone ?? '' }}"
-                                    target="_blank" style="color:#fff; text-decoration: none;">
-                                        <i class="fa fa-comment-o"></i> Chamar
-                                    </a>
-                            </button>
-    
-                                @if (Auth::check())
-                                    <form action="{{ route('lista.esperaApi') }}" method="POST">
-                                        @csrf
-                                        <input type="hidden" name='imovel_id' value="{{ $data->id }}">
-                                        <input type="hidden" name='user_id' value="{{ Auth::user()->id }}">
-                                        <button  style="font-size:14px;" class="btn_chat w-100 text-white mr-3 py-2 border-0" type="submit"
-                                            value="button">
-                                            <i class="fa fa-user-o"></i> Lista de espera
-    
-                                        </button>
-                                    </form>
-                                @endif
-                            </div>
-                            </li>
-
-                            
-                        
+                            <li class="d-inline-block pr-3 detail_prize my-3" style="font-size:20px;"> Preço {{$data->venda  == 1 ? 'Venda' : 'Mensal'}}: <span style="font-size:20px; font-weight:bold; "> R$ {{ str_replace('.',',', $data->price) }} </span></li>
                         </ul>   
                         <div class="row">
-                            <div class="col-6 my-2" style="padding: 0;">
+                            <div class="col-12 col-md-6 my-2" style="padding: 0;">
                                 <span class="" style="font-weight: bold; font-size:14px; color:green;">Iténs</span>
                                 <ul style="list-style:none; text-decoration:none; width:200px; margin:0; padding:0;">
                                     @foreach ( $data->itens as $value )
                                         <li>{{$value->itens->name}}</li> 
                                     @endforeach 
-                                </ul>
-                              
+                                </ul>                              
                             </div>
 
-                            <div class="col-6 my-2">
+                            <div class="col-12 col-md-6 my-2">
                                 <div class="col-6" style="padding: 0;">
                                     <span class="" style="font-weight: bold; font-size:14px; color:red;">Regras</span>
                                     <ul style="list-style:none; text-decoration:none; width:200px; margin:0; padding:0;">
@@ -117,15 +87,40 @@
                                             <li>{{$value->regras->descricao}}</li> 
                                         @endforeach 
                                     </ul>
-                                  
                                 </div>
-                               
                             </div>
+                        
                         </div>
-                    </div>
-                </div>
+                        <div class="col-12 d-flex my-4">
+                            <div class="detail_btn d-flex col-12 my-4">
+                                <button class="btn_chat w-80 text-white mr-1 py-1 border-0" 
+                                type="submit" 
+                                value="button">
+                                    <a href="https://api.whatsapp.com/send?phone={{ $data->user->phone ?? '' }}"
+                                        target="_blank" style="color:#fff; text-decoration: none;">
+                                            <i class="fa fa-comment-o"></i> Chamar
+                                        </a>
+                                </button>
+        
+                                    @if (Auth::check())
+                                        <form action="{{ route('lista.esperaApi') }}" method="POST">
+                                            @csrf
+                                            <input type="hidden" name='imovel_id' value="{{ $data->id }}">
+                                            <input type="hidden" name='user_id' value="{{ Auth::user()->id }}">
+                                            <button  style="font-size:14px;" class="btn_chat w-100 text-white mr-3 py-2 border-0" type="submit"
+                                                value="button">
+                                                <i class="fa fa-user-o"></i> Lista de espera
+        
+                                            </button>
+                                        </form>
+                                    @endif
+                                </div>
+                        </div>
+                    </div>                    
+                </div>                
             </div>
         </div>
+        
     </section>
     <!--End Description -->
 
