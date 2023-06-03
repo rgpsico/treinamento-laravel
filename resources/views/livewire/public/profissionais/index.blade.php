0eee80b9c599e2f77971fc2bba0e27d1bea03751
roger <rogernevesn@gmail.com>
2023-06-03 01:58:59 -0300

    ajusste

--- resources/views/livewire/public/profissionais/index.blade.php
+++ resources/views/livewire/public/profissionais/index.blade.php
@@ -36,7 +36,7 @@
             </div>
        
         
-            <x-list-componentEntregadores :model="$model"  routeUrl="show.profissionais" pasta="imagens/profissionais/" />
+            <x-list-componentProfissionais :model="$model"  routeUrl="show.profissionais" pasta="imagens/entregadores/" />
 
 
 
